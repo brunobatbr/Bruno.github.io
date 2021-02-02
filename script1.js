883e61e36f283fe23de162face74679671a5eeb5
brunobatbr <76232753+brunobatbr@users.noreply.github.com>
2021-02-02 12:26:01 -0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,19 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem') 
+    var data = new Date()
+    //var hora = data.getHours() 
+    var hora = 15   
+    msg.innerHTML = `Agora sao ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //'Bom Dia!'
+        img.src = 'fotomanha.png'  
+    } else if (hora >= 12 && hora < 18) {
+        //'Boa Tarde!'
+        img.src = 'fototarde.png'
+    } else {
+       //"Boa Noite!"
+        img.src = 'fotonoite.png'
+    }
+}     
+  
